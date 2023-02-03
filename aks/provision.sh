a662122363a34ab2f08d3960d37b8eee2bea99da
David Justice <david@devigned.com>
2023-02-03 13:34:07 -0500

    add resource requests and limits

--- aks/provision.sh
+++ aks/provision.sh
@@ -6,13 +6,17 @@ set -o pipefail
 
 export RG=${RG:-"test"}
 export SKIP_PROVISIONING=${SKIP_PROVISIONING:-"false"}
+export KUBE_VERSION=${KUBE_VERSION:-""}
+export EXTRA_ARGS=${EXTRA_ARGS:-""}
 
-if [[ -z "${KUBE_VERSION}" ]]; then echo "error: must set KUBE_VERSION"; exit 1; fi
+if [[ -n "${KUBE_VERSION}" ]]; then
+  export EXTRA_ARGS="${EXTRA_ARGS} -k ${KUBE_VERSION}"
+fi
 
 if [[ "${SKIP_PROVISIONING}" == "false" ]]; then
   rm -rf ./kconfig.yaml
   az group create -n "${RG}" -l southcentralus
-  az aks create -g "${RG}" -n arm-wasi-cluster -k "${KUBE_VERSION}"
+  az aks create -g "${RG}" -n arm-wasi-cluster ${EXTRA_ARGS}
   az aks nodepool add -g "${RG}" --cluster-name arm-wasi-cluster -n armpool --node-count 2 --workload-runtime WasmWasi --node-vm-size Standard_D8ps_v5
   az aks nodepool add -g "${RG}" --cluster-name arm-wasi-cluster -n amdpool --node-count 2 --workload-runtime WasmWasi --node-vm-size Standard_DS2_v2
   az aks get-credentials -g "${RG}" -n arm-wasi-cluster -f kconfig.yaml
